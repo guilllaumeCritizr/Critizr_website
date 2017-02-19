1f504e5b997ad61f18da437e692c5626ab088fcc
Guillaume Boufflers <guillaume.boufflers@critizr.com>
2017-02-19 15:49:04 +0100

    first

---
+++ app.js
@@ -0,0 +1,39 @@
+/*
+ * Module dependencies
+ */
+var express = require('express')
+    , stylus = require('stylus')
+    , nib = require('nib')
+
+var app = express()
+function compile(str, path) {
+    return stylus(str)
+        .set('filename', path)
+        .use(nib())
+}
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+app.use(express.logger('dev'))
+app.use(stylus.middleware(
+    { src: __dirname + '/public'
+        , compile: compile
+    }
+))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/MRBRICOLAGE/ABCDEF', function (req, res) {
+//    res.end('Hi there!')
+    res.writeHead(301,
+        {Location: 'http://www.mr-bricolage.fr/?magasin=Lillers&cz_open=true'}
+    );
+    res.end();
+})
+
+app.get('/CARREFOUR/ABCDEF', function (req, res) {
+//    res.end('Hi there!')
+    res.writeHead(301,
+        {Location: 'http://restaurant.flunch.fr/lille/flunch-lille-gare/?store=107&origin=50.62925%2C3.057256&cz_open=true'}
+    );
+    res.end();
+})
+app.listen(3000)
